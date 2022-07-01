6b2292ed83542fa2a44adcec663e3a7e092d5a9c
Yannick Graffi <yann001@hotmail.com>
2022-07-01 14:41:25 +0200

    add try-catch. make api restful

--- back/app.js
+++ back/app.js
@@ -4,10 +4,10 @@ require('./config/connexion.js')()
 
 const app = express();
 
-app.use(express.json());
-
 const lodge = require('./routes/lodge')
 
-app.use('/lodge', lodge) // page to see details on a particular lodge
+app.use(express.json());
+
+app.use('/lodges', lodge) // page to see details on a particular lodge
 
 app.listen (process.env.PORT, () => console.log(`Server listening on port ${process.env.port}`))
--- back/routes/lodge.js
+++ back/routes/lodge.js
@@ -2,45 +2,61 @@ const express = require('express')
 const router = express.Router();
 const Lodge = require('../models/Lodge')
 
-
 router.get('/', async (req, res) =>{
-    const result = await Lodge.find()
-
-    res.status(200).send(result)
+    try {
+        const result = await Lodge.find()
+        res.status(200).send(result)        
+    } catch (err) {
+        console.error(err.message);        
+    }
 })
 
 router.get('/:id', async (req, res) =>{
-    const result = await Lodge.findById(req.params.id)    
-    res.status(200).send(`Correctly found one lodge ${result}`)
+    try {
+        const result = await Lodge.findById(req.params.id)    
+        res.status(200).send({message:'Correctly found one lodge', data:result})        
+    } catch (err) {
+        console.error(err.message);        
+    }
 })
 
-router.post('/add-lodge', async (req, res) =>{
-    const {title, description, price}= req.body
-    const lodge = new Lodge({
-            title,
-            description,
-            price,
+router.post('/', async (req, res) =>{
+    try {
+        const {title, description, price}= req.body
+        const lodge = new Lodge({
+                title,
+                description,
+                price,
+            })
+        await lodge.save()  
+        res.status(201).send({message:'correctly created',data: lodge})        
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+router.put('/:id', async (req, res) =>{
+    try {
+        const id = req.params.id
+        await Lodge.findByIdAndUpdate(id, req.body, {
+            new:true,
         })
-
-    await lodge.save()  
-      
-    res.status(201).send(`correctly created ${lodge}`)
-})
-
-router.put('/update-lodge/:id', (req, res) =>{
-    const id = req.params.id
-
+        res.send('Lodge correctly updated')            
+    } catch (err) {
+        console.error(err.message);
+    }
 
 })
 
-router.delete('/delete-lodge/:id', async (req, res) =>{
-    const id = req.params.id
+router.delete('/:id', async (req, res) =>{
+    try {
+        const id = req.params.id
+        await Lodge.findByIdAndDelete(id)
+        res.send('logement correctement supprimé')        
+    } catch (err) {
+        console.log(err.message);
+    }
 
-    await Lodge.findByIdAndDelete(id)
-
-    res.send(`logement correctement supprimé`)
 })
-
-
 
 module.exports = router
